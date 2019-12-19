f895db7d9b701f1a9483dde6c02b0b7124dae304
svarzee <svarzee@gmail.com>
2019-12-19 14:07:59 +0100

    Add benchs for dart with copy

--- benchmark/hamt_bench.dart
+++ benchmark/hamt_bench.dart
@@ -4,41 +4,15 @@ import 'dart:math';
 import 'package:benchmark_harness/benchmark_harness.dart';
 import 'package:functional_collections/src/hamt.dart';
 
-class AddBenchmark extends BenchmarkBase {
-  AddBenchmark(bool functional)
-      : this.functional = functional,
-        super((functional ? "functional" : "dart") + " add");
-
-  final bool functional;
-
-  List<int> values;
-  int N = 10000;
-
-  @override
-  void setup() {
-    final random = Random(0);
-    values = List<int>.generate(N, (idx) => random.nextInt(1 << 32));
-  }
-
-  @override
-  void run() {
-    if (functional) {
-      values.fold(Hamt(), (set, val) => set.add(val, val));
-    } else {
-      values.fold(HashSet(), (set, val) => set..add(val));
-    }
-  }
-}
-
 class ContainsBenchmark extends BenchmarkBase {
-  ContainsBenchmark(bool functional)
+  ContainsBenchmark(bool functional, this.N)
       : this.functional = functional,
         super((functional ? "functional" : "dart") + " contains");
 
   final bool functional;
 
   List<int> values;
-  int N = 10000;
+  int N;
 
   Hamt funSet;
   Set dartSet;
@@ -61,15 +35,51 @@ class ContainsBenchmark extends BenchmarkBase {
   }
 }
 
-class RemoveBenchmark extends BenchmarkBase {
-  RemoveBenchmark(bool functional)
+class AddBenchmark extends BenchmarkBase {
+  AddBenchmark(bool functional, bool copy, this.N)
       : this.functional = functional,
-        super((functional ? "functional" : "dart") + " remove");
+        this.copy = copy,
+        super((functional ? "functional" : "dart") +
+            (copy ? " copy" : "") +
+            " add");
 
   final bool functional;
+  final bool copy;
 
   List<int> values;
-  int N = 10000;
+  int N;
+
+  @override
+  void setup() {
+    final random = Random(0);
+    values = List<int>.generate(N, (idx) => random.nextInt(1 << 32));
+  }
+
+  @override
+  void run() {
+    if (functional) {
+      values.fold(Hamt(), (set, val) => set.add(val, val));
+    } else if (copy) {
+      values.fold(HashSet(), (set, val) => HashSet.of(set)..add(val));
+    } else {
+      values.fold(HashSet(), (set, val) => set..add(val));
+    }
+  }
+}
+
+class RemoveBenchmark extends BenchmarkBase {
+  RemoveBenchmark(bool functional, bool copy, this.N)
+      : this.functional = functional,
+        this.copy = copy,
+        super((functional ? "functional" : "dart") +
+            (copy ? " copy" : "") +
+            " remove");
+
+  final bool functional;
+  final bool copy;
+
+  List<int> values;
+  int N;
 
   Hamt funSet;
   Set dartSet;
@@ -86,6 +96,8 @@ class RemoveBenchmark extends BenchmarkBase {
   void run() {
     if (functional) {
       values.fold(funSet, (set, val) => set.remove(val));
+    } else if (copy) {
+      values.fold(dartSet, (set, val) => HashSet.of(set)..remove(val));
     } else {
       values.fold(dartSet, (set, val) => set..remove(val));
     }
@@ -93,12 +105,14 @@ class RemoveBenchmark extends BenchmarkBase {
 }
 
 main() {
-  AddBenchmark(false).report();
-  AddBenchmark(true).report();
+  ContainsBenchmark(false, 10000).report();
+  ContainsBenchmark(true, 10000).report();
 
-  ContainsBenchmark(false).report();
-  ContainsBenchmark(true).report();
+  AddBenchmark(false, false, 10000).report();
+  AddBenchmark(false, true, 10000).report();
+  AddBenchmark(true, false, 10000).report();
 
-  RemoveBenchmark(false).report();
-  RemoveBenchmark(true).report();
+  RemoveBenchmark(false, false, 10000).report();
+  RemoveBenchmark(false, true, 10000).report();
+  RemoveBenchmark(true, false, 10000).report();
 }
