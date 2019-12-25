43a6d6d49526b692b3c989cb1df8c673f34ae90d
svarzee <svarzee@gmail.com>
2019-12-25 03:29:18 +0100

    Add set methods

--- lib/src/set.dart
+++ lib/src/set.dart
@@ -1,5 +1,4 @@
 import 'package:functional_collections/src/hamt.dart';
-import 'package:functional_collections/src/list.dart';
 import 'package:functional_collections/src/tuple.dart';
 
 class FSet<T> extends Iterable<T> {
@@ -7,11 +6,19 @@ class FSet<T> extends Iterable<T> {
 
   FSet() : hamt = Hamt();
 
+  FSet._(Hamt<T, T> hamt) : hamt = hamt;
+
   FSet.from(Iterable<T> keyVals)
       : hamt = Hamt()
             .addAll(keyVals.map((value) => Tuple2(value, value)).toList());
 
-  bool containsKey(T value) => hamt.contains(value);
+  @override
+  bool contains(Object value) => hamt.contains(value);
+
+  FSet<T> add(T value) => FSet._(hamt.add(value, value));
+
+  FSet<T> addAll(Iterable<T> values) =>
+      FSet._(hamt.addAll(values.map((value) => Tuple2(value, value)).toList()));
 
   @override
   Iterator<T> get iterator => hamt.entries().map((item) => item.val1).iterator;
