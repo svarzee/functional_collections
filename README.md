144f3b6bee1a434995ca7a5bf6e5c7ccc2dea6f7
svarzee <svarzee@gmail.com>
2019-12-25 23:21:29 +0100

    Improve README

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Description
 ---
-Functional collections.
+Functional collections are immutable, adding or removing items efficiently creates a new collection. You can choose from: `FOption`, `FList`, `FSet`, `FMap`, `FTuple`.
--- example/main.dart
+++ example/main.dart
@@ -4,5 +4,5 @@ void main() {
   FOption<int> opt = 1 > 2 ? FNone() : FSome(1);
   FList.from([1, 2, 3]).append(4).prepend(0);
   FSet.from([1, 2, 3]).add(4);
-  FMap.from([Tuple2(1, "a"), Tuple2(2, "b")]).put(3, "c");
+  FMap.from([FTuple2(1, "a"), FTuple2(2, "b")]).put(3, "c");
 }
