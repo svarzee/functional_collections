6a9d9d9de1bd8674e60bee60fba31de2a8bcb5af
svarzee <svarzee@gmail.com>
2019-12-25 23:12:14 +0100

    Add example

--- example/README.md
+++ example/README.md
@@ -1,3 +1,3 @@
 Description
 ---
-Immutable collections.
+Example.
---
+++ example/main.dart
@@ -0,0 +1,8 @@
+import 'package:functional_collections/functional_collections.dart';
+
+void main() {
+  FOption<int> opt = 1 > 2 ? FNone() : FSome(1);
+  FList.from([1, 2, 3]).append(4).prepend(0);
+  FSet.from([1, 2, 3]).add(4);
+  FMap.from([Tuple2(1, "a"), Tuple2(2, "b")]).put(3, "c");
+}
